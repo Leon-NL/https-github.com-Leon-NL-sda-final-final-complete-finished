ef7872b4d0c06533e808282a09499089773f634e
leonhulsebos <leonhulsebos@outlook.com>
2023-09-25 14:29:29 +0200

    Add dobot control and dobot library

---
+++ dobot_Control.py
@@ -0,0 +1,29 @@
+from pydobot import Dobot
+from serial.tools import list_ports
+import time
+
+class dobotCommand():
+    def __init__(self):
+        available_ports = list_ports.comports()
+        print('available ports: {0}'.format([x.device for x in available_ports]))
+        # if available_ports.__len__() == 0 or not str([x.device for x in available_ports]).__contains__('COM'):
+        #     raise Exception('Ojee, geen Dobot gevonden! \r\nBeschiktbare poorten: {0}'.format([x.device for x in available_ports]))
+        # port = available_ports[0].device
+        self.mydevice = Dobot(port="COM3", verbose=False)
+        self.mydevice.clear_alarms()
+        
+    def home(self,):
+        self.mydevice.speed(100, 100)
+        # self.mydevice.clear_alarms()
+        self.mydevice.home()
+
+    def setSpeed(self, speed: int):
+        self.mydevice.speed(speed, speed)
+
+    def move(self, x, y, z, r, wait: bool):
+        self.mydevice.move_to(x, y, z, r, wait=wait)
+        time.sleep(0.1)
+
+    def close(self):
+        self.mydevice.close()
+        
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from dobot_Control import *
 from Vision import *
+import time
 
 if ( __name__ == "__main__"):
-    print("test")
+    robot = dobotCommand()
+
+    robot.home()
+    time.sleep(5)
+    robot.move()
+    robot.close()
